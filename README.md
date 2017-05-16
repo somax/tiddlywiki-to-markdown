850cd9516cc40a961773855153c6b838f94820a8
somax <somaxj@163.com>
2017-05-16 16:12:04 +0800

    First Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# tiddlywiki2markdown
+
+Converting Tiddlywiki Wikitext to Markdown
+
+## install
+
+```
+git clone git https://github.com/somax/tiddlywiki-to-markdown.git
+
+npm i
+```
+
+## usage
+
+1. export all contents to json file `tiddlers.json`
+2. mkdir `output` folder and put `tiddlers.json` file in. 
+3. run `node .`
+4. checkout `output` folder
---
+++ index.js
@@ -0,0 +1,84 @@
+const tiddlers = require('./input/tiddlers.json');
+const fs = require('fs');
+const makeDir = require('make-dir');
+
+const outputPath = 'output/';
+const outputFile = outputPath + 'output.md';
+const outputWS = fs.createWriteStream(outputFile, {
+    flags: 'a' // 'a' means appending (old data will be preserved)
+})
+
+function output(l) {
+    // console.log(l);
+    outputWS.write(l + '\n');
+}
+
+
+const convertor = {
+    "image/png": (tiddler) => {
+        // write to file
+        var data = tiddler.text.replace(/^data:image\/\w+;base64,/, "");
+        var buf = new Buffer(data, 'base64');
+        fs.writeFileSync(outputPath + tiddler.title, buf);
+    },
+    "image/svg+xml": (tiddler) => {
+        // write to file
+        fs.writeFileSync(outputPath + tiddler.title, tiddler.text)
+    },
+    "text/vnd.tiddlywiki": (tiddler) => {
+        let lines = tiddler.text.split('\n');
+
+        for (let l of lines) {
+            let _l = l
+                .replace(/^by \[.+/, '')
+                .replace(/^;by \[.+/, '')
+                .replace(/^#/, '* ')
+                .replace(/^\*;/, '* ')
+                .replace(/^\*/, '* ')
+                .replace(/^\*   /, '* ')
+                .replace(/^\*  /, '* ')
+                .replace(/^:/, '  * ')
+                .replace(/^!!!!/, '#### ')
+                .replace(/^!!!/, '### ')
+                .replace(/^!!/, '## ')
+
+                .replace(/''(.+)''/g, '*$1*')
+                .replace(/\[\[(.+)\|(.+)\]\]/, '[$1]($2)')
+                .replace(/\[\[(.+)\]\]/, '[$1]($1)')
+
+                .replace(/\[img\[(.+)\]\]/, '![]($1)')
+
+            output(_l);
+        }
+    }
+}
+
+
+
+
+function convert(tiddler) {
+    output(`# ${tiddler.title}
+    `)
+
+    let tags = tiddler.tags ? tiddler.tags.replace(/\[\[(.+)\]\]/, '[$1]($1)') : ''
+    output(`> tags: ${tags}
+    `)
+
+    if (Object.keys(convertor).indexOf(tiddler.type) !== -1) {
+        convertor[tiddler.type](tiddler);
+    } else {
+        output(tiddler.text);
+
+    }
+
+    output('\n-------------\n')
+}
+
+
+makeDir(outputPath).then(() => {
+    fs.writeFileSync(outputFile,'');
+
+    for (let t of tiddlers) {
+        convert(t);
+    }
+})
